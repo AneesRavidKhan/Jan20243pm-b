6a5cbd73a1802671a51e7f87ea023c53ac5799ec
Anees Khan <aneeskhan@gamutgurus.com>
2024-03-06 16:11:53 +0530

    Revert "added new login code changes"
    This reverts commit 628c52a85fc68889ee428916fbe732fd5c64e918.

--- login.java
+++ login.java
@@ -10,7 +10,7 @@
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
 
-    <button type="submit">Login Now</button>
+    <button type="submit">Login</button>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
@@ -18,6 +18,6 @@
 
   <div class="container" style="background-color:#f1f1f1">
     <button type="button" class="cancelbtn">Cancel</button>
-    <span class="psw">Not Remember? <a href="#">password?</a></span>
+    <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
 </form>
